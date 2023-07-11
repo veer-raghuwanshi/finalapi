38fd1ddf5de2c8790dc51648066c2c7a668882fa
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-07-12 01:35:17 +0530

    Update old api register connection.js

--- modals/connection.js
+++ modals/connection.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-var url = "mongodb+srv://veer_raghuwanshi:<password>@finalapi.r8bkae2.mongodb.net/final_api"
+var url = "mongodb+srv://veer_raghuwanshi:1234@cluster1.vtznpbg.mongodb.net/Universaldatabase"
 mongoose.connect(url)
 // var db = mongoose.connection
 console.log("Database connection successfully Done!!")
