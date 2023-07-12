2cbdf2b15ea7428f197a403feffd92dfa0b7abdd
Veerendra Raghuwanshi <68636328+veer-raghuwanshi@users.noreply.github.com>
2023-07-12 09:09:26 +0530

    Update connection.js

--- modals/connection.js
+++ modals/connection.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-var url = "mongodb+srv://veer_raghuwanshi:1234@finalapi.r8bkae2.mongodb.net/testingapi"
+var url = "mongodb+srv://veer_raghuwanshi:1234@finalapi.r8bkae2.mongodb.net/finalapi"
 mongoose.connect(url)
 // var db = mongoose.connection
 console.log("Database connection successfully Done!!")
